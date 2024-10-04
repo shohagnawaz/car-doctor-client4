43a460b8799234805e94860240c582b42a35dfdc
shohagnawaz <snawazdigital@gmail.com>
2024-10-04 18:39:24 +0600

    banner

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Banner from "../Banner/Banner";
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <h2>Md. Shah Nawaz Shohag</h2>
+        </div>
+    );
+};
+
+export default Home;
